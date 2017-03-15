d3c485b400613e0c857e3997fd8af5b813803c3e
hoangvhh <vuhuyhoang.bk@gmail.com>
2017-03-15 14:54:20 +0700

    update VIETTEL IDC 2

---
+++ stun/stunclient.hpp
@@ -0,0 +1,13 @@
+/*
+ * stunclient.hpp
+ *
+ *  Created on: Jan 18, 2017
+ *      Author: hoang
+ */
+
+#ifndef STUNCLIENT_HPP_
+#define STUNCLIENT_HPP_
+
+int stunclient(int argc, char** argv);
+
+#endif /* STUNCLIENT_HPP_ */
